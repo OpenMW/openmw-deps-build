efa5d28e30b8f717a012a693fbb8fbe01565fdcb
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2025-09-13 15:56:27 -0500

    [RECAST] Copy from port, use openmw sha

--- ports/recastnavigation/portfile.cmake
+++ ports/recastnavigation/portfile.cmake
@@ -1,20 +1,27 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO recastnavigation/recastnavigation
-  REF 40ec6fcd6c0263a3d7798452aee531066072d15d
-  SHA512 e5b8d84b1f399faceaed27286f503991536298da8be4dfaeca9a32b2628c8316af6245dbe46517e39821b8e4f2634a9e1de23e992cb7842f4b38368b81ec0302
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO recastnavigation/recastnavigation
+    REF c393777d26d2ff6519ac23612abf8af42678c9dd
+    SHA512 bcf39c72fed335b7b48fa245088d135e512855f9e8cd3567527ffed473f82a7914b7e7c15d6cdda74d4a2e7624bb4acc132b572d8a33882c9a3d3fa4fb7ff59a
 )
 vcpkg_cmake_configure(
-  SOURCE_PATH "${SOURCE_PATH}"
-  OPTIONS
-    -DBUILD_SHARED_LIBS=ON
-    -DRECASTNAVIGATION_DEMO=OFF
-    -DRECASTNAVIGATION_TESTS=OFF
-    -DRECASTNAVIGATION_EXAMPLES=OFF
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DRECASTNAVIGATION_DEMO=OFF
+        -DRECASTNAVIGATION_TESTS=OFF
+        -DRECASTNAVIGATION_EXAMPLES=OFF
+
 )
+
 vcpkg_cmake_install()
+
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/recastnavigation)
+
 vcpkg_fixup_pkgconfig()
+
 vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.txt")
